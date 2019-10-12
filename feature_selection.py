512b217f210394e7c28f7619255a48b5c0f64ae4
SadyRifat <sadyrifat@gmail.com>
2019-10-12 22:34:55 +0600

    common features add

--- feature_selection.py
+++ feature_selection.py
@@ -3,17 +3,65 @@ from sklearn.feature_selection import RFE
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+import pandas as pd
 
-def recursiveFeatureElimination(X, Y):
+
+def findCommonFeature(featureLIST):
+    featureSET = []
+    for item in featureLIST:
+        featureSET.append(set(item))
+
+    setItem = featureSET.pop()
+    for item in featureSET:
+        setItem = setItem.intersection(item)
+
+    return setItem
+
+def findIntersectionFeature(Xfeature, commonFeatures):
+    intersect = set(Xfeature) - set(commonFeatures)
+    return intersect
+
+def combinedWithFeature(rank, data):
+    dictionary = {}
+    index = 0
+    for feature in data.columns:
+        dictionary[feature] = rank[index]
+        index = index + 1
+    return dictionary
+
+def getFeatureName(dict, asc):
+    sortedItem = sorted(dict.items(), key=lambda kv: (kv[1], kv[0]))
+    topFeatures = []
+    for key, item in sortedItem:
+        topFeatures.append(key)
+    if(asc):
+        return topFeatures
+    else:
+        topFeatures.reverse()
+        return topFeatures
+
+def recursiveFeatureElimination(X, Y, featureTaken):
     model = LogisticRegression()
     rfe = RFE(model, 15)
     fit = rfe.fit(X, Y)
+    rank = fit.ranking_
+    rankedFeature = combinedWithFeature(rank, X)
+    topFeatures = getFeatureName(rankedFeature, True)
+    return topFeatures[:featureTaken]
 
-def featureExtraction(X, Y):
+def featureExtraction(X, Y, featureTaken):
     model = ExtraTreesClassifier()
     fit = model.fit(X, Y)
+    rank = fit.feature_importances_
+    rankedFeature = combinedWithFeature(rank, X)
+    topFeatures = getFeatureName(rankedFeature, False)
+    return topFeatures[:featureTaken]
 
-
-def univarientFeatureExtraction(X, Y):
+def univariateFeatureExtraction(X, Y, featureTaken):
     model = SelectKBest(score_func=chi2, k=15)
     fit = model.fit(X, Y)
+    rank = fit.scores_
+    rankedFeature = combinedWithFeature(rank, X)
+    topFeatures = getFeatureName(rankedFeature, False)
+    return topFeatures[:featureTaken]
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 from Data_Retrieve import makeDataset
 from Pre_Process import word2num
 from Pre_Process import handlingMissingData as md
@@ -12,7 +14,7 @@ warnings.filterwarnings("ignore")
 
 df = pd.read_csv('Accident-Data.csv')
 Y = df['Accident_Severity']
-df = df.drop(['X', 'Y', 'Road_No', 'Accident_Severity'], axis=1)
+df = df.drop(['Unnamed: 0', 'X', 'Y', 'Road_No', 'Accident_Severity'], axis=1)
 X = df
 
 
@@ -23,9 +25,22 @@ for feature in df.columns:
 
 
 X['Driver_Age'] = md.replaceByMean(X['Driver_Age'])
+Xfeature = []
+for feature in X.columns:
+    Xfeature.append(feature)
 
-fs.featureExtraction(X, Y)
-fs.recursiveFeatureElimination(X, Y)
-fs.univarientFeatureExtraction(X, Y)
+featureExtraction = fs.featureExtraction(X, Y, 15)
+recursiveFeature = fs.recursiveFeatureElimination(X, Y, 15)
+univariateFeature = fs.univariateFeatureExtraction(X, Y, 15)
 
-print('success')
+featureLIST = []
+featureLIST.append(featureExtraction)
+featureLIST.append(recursiveFeature)
+featureLIST.append(univariateFeature)
+
+commonFeatures = fs.findCommonFeature(featureLIST)
+intersectionFeature = fs.findIntersectionFeature(Xfeature, commonFeatures)
+for feature in intersectionFeature:
+    X = X.drop(feature, axis=1)
+
+print("success")
